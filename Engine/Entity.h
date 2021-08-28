77c7c1a93ed6a7ee96da17e72249afcea4124ff6
SkyllarRisen <20288996+SkyllarRisen@users.noreply.github.com>
2021-08-28 12:17:38 +0200

    Moved Entity static counter to protected access.

--- Engine/Entity.h
+++ Engine/Entity.h
@@ -10,7 +10,7 @@ public:
     Entity() = delete;
     Entity(std::string type, Vec2D pos = Vec2D(0, 0), Vec2D vel = Vec2D(0,0));
     Entity(std::string type, Vec3D pos, Vec3D vel = Vec3D(0,0,0));
-    static unsigned int numberOfEntities;
+    
 
 
     std::string Type() const;
@@ -27,6 +27,8 @@ protected:
     void changeVel(const double x, const double y, const double z);
     void changeVel(const Vec2D& xy, const double z = 0);
     void changeVel(const Vec3D& xyz);
+
+    static unsigned int numberOfEntities;
 
 private:
 
